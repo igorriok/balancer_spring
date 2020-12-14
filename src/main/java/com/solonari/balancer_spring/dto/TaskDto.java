106560f0d14d7a7f7c6ba9eee8dee4639c53c5bf
ISOLONARI <igor.solonari@orange.md>
2020-12-14 20:06:55 +0200

    save task

--- src/main/java/com/solonari/balancer_spring/dto/TaskDto.java
+++ src/main/java/com/solonari/balancer_spring/dto/TaskDto.java
@@ -3,12 +3,15 @@ package com.solonari.balancer_spring.dto;
 import com.solonari.balancer_spring.entities.TaskEntity;
 
 public class TaskDto {
+	
+	public Long id;
 	public String taskName;
 	public String groupName;
 	
 	public TaskDto() {}
 	
 	public TaskDto(TaskEntity taskEntity) {
+		this.id = taskEntity.id;
 		this.taskName = taskEntity.name;
 		if (taskEntity.group != null) {
 			this.groupName = taskEntity.group.name;
