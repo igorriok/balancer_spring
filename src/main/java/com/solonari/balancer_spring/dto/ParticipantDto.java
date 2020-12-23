d55fc1268cf1df19e4f187bd7c700b412f1d5677
ISOLONARI <igor.solonari@orange.md>
2020-12-23 21:34:20 +0200

    group participants

---
+++ src/main/java/com/solonari/balancer_spring/dto/ParticipantDto.java
@@ -0,0 +1,22 @@
+package com.solonari.balancer_spring.dto;
+
+import com.solonari.balancer_spring.entities.UserEntity;
+
+public class ParticipantDto {
+	
+	public String nickName;
+	public String email;
+	
+	
+	public ParticipantDto() {}
+	
+	public ParticipantDto(String nickName, String email) {
+		this.nickName = nickName;
+		this.email = email;
+	}
+	
+	public ParticipantDto(UserEntity userEntity) {
+		this.nickName = userEntity.nickName;
+		this.email = userEntity.username;
+	}
+}
