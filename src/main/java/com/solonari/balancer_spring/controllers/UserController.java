6f1e0ab73311ebdd8c61a7a5190f5bba353915c8
ISOLONARI <igor.solonari@orange.md>
2021-01-10 10:27:33 +0200

    change password

--- src/main/java/com/solonari/balancer_spring/controllers/UserController.java
+++ src/main/java/com/solonari/balancer_spring/controllers/UserController.java
@@ -6,20 +6,17 @@ import com.solonari.balancer_spring.services.UsersDetailsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
 @RestController
-public class NickNameController {
+public class UserController {
 	
-	private static final Logger log = LoggerFactory.getLogger(NickNameController.class);
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	private final UsersDetailsService usersDetailsService;
 	
-	public NickNameController(UsersDetailsService usersDetailsService) {
+	public UserController(UsersDetailsService usersDetailsService) {
 		this.usersDetailsService = usersDetailsService;
 	}
 	
@@ -45,5 +42,20 @@ public class NickNameController {
 		userEntity = usersDetailsService.saveUser(userEntity);
 		
 		return ResponseEntity.ok().body(userEntity.nickName);
+	}
+	
+	
+	@PostMapping(value = "/password")
+	public ResponseEntity<String> setPassword(@RequestBody String password, Principal token) {
+		
+		log.info("Set password for: {}", token.getName());
+		
+		UserEntity userEntity = usersDetailsService.getUserByUsername(token.getName());
+		
+		userEntity.password = password;
+		
+		usersDetailsService.saveUser(userEntity);
+		
+		return ResponseEntity.ok().body("saved");
 	}
 }
